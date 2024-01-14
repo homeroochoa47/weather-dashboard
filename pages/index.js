5d92629c038d092f18897bd18f2b46cd047f9066
Homero Ochoa <homeroochoa47@gmail.com>
2024-01-13 20:21:14 -0800

    add DailyConditions component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import MainForecastPanel from '@/components/MainForecastPanel'
+import DailyConditions from '@/components/DailyConditions'
 import { fetchDailyForecastData } from '@/utils/fetchDailyForecastData'
 
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
 import React, { useEffect, useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function Dashboard() {
   // State is later set to an array with two objects corresponding to day and night weather
@@ -14,7 +15,6 @@ export default function Dashboard() {
   // fetch data on initial page load, run timer to refetch using setInterval
   useEffect(() => {
     const fetchData = async () => {
-      console.log('fetching');
       setDailyForecastData(await fetchDailyForecastData());
     };
 
@@ -27,12 +27,12 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <main className={`flex min-h-screen items-center justify-between ${inter.className} bg-gradient-to-br from-[#027baa] to-[#f99c9a] text-black`}>
+    <main className={`flex min-h-screen items-center justify-between ${montserrat.className} bg-gradient-to-br from-[#027baa] to-[#f99c9a] text-black`}>
     
       <MainForecastPanel dailyForecastData={dailyForecastData} />
 
-      <div id="forecast-overview-panel" className='flex flex-col'>
-
+      <div id="forecast-overview-panel" className='flex flex-col w-[70%] mx-auto max-w-4xl h-[90vh] justify-evenly'>
+        <DailyConditions dailyForecastData={dailyForecastData} />
       </div>
 
     </main>
